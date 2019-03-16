0e73c22eaf61b9b1aa07541c54a626c520315b80
Rishabh Sharma <rishabh2241991@gmail.com>
2019-03-16 21:00:38 +0530

    Create SambaVersion_Detection.py

---
+++ SambaVersion_Detection.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+import re
+ip = raw_input("Enter IP Address: ")
+port = raw_input("Enter SMB Port Number: ")
+
+os.system("tcpdump -s0 -A -i tap0 port 139 -c 20 >> smbversion.txt &")
+os.system("echo exit | smbclient -L \\\\"+ip)
+with open ('smbversion.txt','r') as file:
+        version = file.read()
+        #print version
+        searchobj = re.search(r'(Samba [\d].[\d].[\d\w].)', version, re.I)
+        if searchobj:
+                print "Samba Version: "+searchobj.group(1)
+        else:
+                print "Version not found."
